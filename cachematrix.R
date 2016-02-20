550f0eeab77d7d6c38334f18d7775947f5d862c0
fflv1977 <fflv1977@gmail.com>
2016-02-20 17:58:42 -0500

    FLopez_MyAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  
+  ima <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    ima <<- NULL
+  }
+  get <- function() x
+  
+  setsolve <- function(solve) ima <<- solve
+  getsolve <- function() ima
+  
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+  ima <- x$getsolve()
+  if(!is.null(ima)) {
+    message("getting cached data")
+    return(ima)
+  }
+  data <- x$get()
+  ima <- solve(data, ...)
+  x$setsolve(ima)
+  ima
 }
+
